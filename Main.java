fd9a12361346b08e8af0a1ff621612685522d7fb
evgens12348 <evgen.s12348@gmail.com>
2017-04-12 00:13:57 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,37 @@
+package com.gmail.s12348.evgen;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) {
+		File fileOne = new File("FileOne.txt");
+		File fileTwo = new File("FileTwo.txt");
+		File fileTree = new File("FileTree.txt");
+		try (BufferedReader bfrOne = new BufferedReader(new FileReader(fileOne));
+				BufferedReader bfrTwo = new BufferedReader(new FileReader(fileTwo));
+				BufferedWriter bfwOne = new BufferedWriter(new FileWriter(fileTree))) {
+			if (fileTree.exists() == false) {
+				fileTree.createNewFile();
+			}
+			String[] masOne = bfrOne.readLine().split("[ ,.!?]");
+			System.out.println(masOne.length);
+			String[] masTwo = bfrTwo.readLine().split("[ ,.!?]");
+			System.out.println(masTwo.length);
+			for (int i = 0; i < masOne.length; i++) {
+				for (int j = 0; j < masTwo.length; j++) {
+					if (masOne[i].compareToIgnoreCase(masTwo[j]) == 0) {
+						bfwOne.write(masOne[i] + " ");
+					}
+				}
+			}
+		} catch (IOException e) {
+			System.out.println(e);
+		}
+	}
+}
